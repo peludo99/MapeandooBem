bb645c6fa60ebdd72671e228e46f3418a71a972b
CauandoP <148398651+peludo99@users.noreply.github.com>
2024-09-27 20:14:56 -0300

    tela cadastro

--- app/src/main/java/com/peludo/mapeando0bem/TelaCadastrarActivity.kt
+++ app/src/main/java/com/peludo/mapeando0bem/TelaCadastrarActivity.kt
@@ -5,29 +5,26 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class TelaLoginActivity : AppCompatActivity() {
+class TelaCadastrarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tela_login)
+        setContentView(R.layout.activity_tela_cadastrar)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-
-
-        val btniniciar = findViewById<TextView>(R.id.cadastrase)
+        val btniniciar = findViewById<TextView>(R.id.login)
 
         btniniciar.setOnClickListener {
 
 
-            val intent = Intent(this, TelaCadastrarActivity::class.java)
+            val intent = Intent(this, TelaLoginActivity::class.java)
             startActivity(intent)
         }
     }
